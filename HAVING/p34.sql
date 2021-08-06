bda18fd56ea22fe6d378de8e3571f9b532d47a85
Alex <alexbean95@gmail.com>
2021-08-06 08:49:51 -0600

    HAVING complete

---
+++ HAVING/p34.sql
@@ -0,0 +1,10 @@
+SELECT c.CustomerID, c.CompanyName, o.OrderID, SUM(od.unitPrice * od.Quantity) AS TotalsWithoutDiscount, SUM(od.unitPrice * od.Quantity * (1-od.Discount)) AS TotalsWithDiscount
+	FROM Customers c
+    JOIN Orders o
+    ON c.CustomerID = o.CustomerID
+    JOIN OrderDetails od
+    ON o.OrderID = od.OrderID
+    WHERE OrderDate LIKE '2016%'
+    GROUP BY c.CustomerID
+    HAVING (TotalsWithDiscount >= 10000)
+    ORDER BY TotalsWithDiscount DESC;
