88edd613a20472eb9cede2919c352cfdae198e4e
Alex <alexbean95@gmail.com>
2021-08-13 18:45:47 -0600

    First new problem set complete

--- Unused Problems/p35.sql
+++
@@ -1,6 +0,0 @@
-SELECT EmployeeID, OrderID, OrderDate
-	FROM orders
-    WHERE OrderDate = LAST_DAY(SUBSTRING(OrderDate, 1, 10))
-    ORDER BY EmployeeID;
-
--- SUBSTR does the same thing, but is shorter.
