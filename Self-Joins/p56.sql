e514f1e5b04434b1868c4749431cbf1ed87167ed
Alex <alexbean95@gmail.com>
2021-08-13 23:44:55 -0600

    Problem 56 gets the exactly right number of rows now

--- Self-Joins/p56.sql
+++ Self-Joins/p56.sql
@@ -4,5 +4,6 @@ SELECT o.CustomerID, o.OrderID AS InitialOrderID, DATE(o.orderDate) AS InitialOr
 		ON c.CustomerID = o.CustomerID
 	JOIN orders od
 		ON o.CustomerID = od.CustomerID
-	WHERE TIMESTAMPDIFF(SECOND, o.OrderDate, od.OrderDate) BETWEEN 1 AND 432000 OR DATEDIFF(od.OrderDate, o.OrderDate) = 5
+	WHERE TIMESTAMPDIFF(SECOND, o.OrderDate, od.OrderDate) BETWEEN 1 AND 432000 OR DATEDIFF(od.OrderDate, o.OrderDate) = 5 OR (o.OrderDate = od.OrderDate AND o.OrderID != od.OrderID)
+    GROUP BY o.OrderDate, od.OrderDate
 	ORDER BY c.CustomerID, InitialOrderID;
