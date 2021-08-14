60b36d0d271420e542f5758f94a6caa822785da7
Alex <alexbean95@gmail.com>
2021-08-14 16:21:28 -0600

    Reorganized HAVING folder and made note of p38 and p39

--- HAVING/p38.sql
+++ HAVING/p38.sql
@@ -1,5 +1,3 @@
--- This is a prerequisite for p39, which is why I did p38 first, even though it wasn't listed
-
 SELECT OrderID
 	FROM OrderDetails
 	WHERE Quantity >= 60
--- Subquery/p39.sql
+++ Subquery/p39.sql
@@ -1,3 +1,5 @@
+-- Since this problem requires understanding p38, I did p38 first.  I later realized p38 was listed later on and reorganized accordingly.
+
 SELECT *
 	FROM  orderDetails
     WHERE OrderID IN (
