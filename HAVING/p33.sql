fe926ee65a894aba50a24d9b3fd1912b5c128dfd
Alex <alexbean95@gmail.com>
2021-08-24 09:17:05 -0600

    Removed extra column in p33

--- HAVING/p33.sql
+++ HAVING/p33.sql
@@ -1,4 +1,4 @@
-SELECT c.CustomerID, c.CompanyName, o.OrderID, SUM(od.unitPrice * od.Quantity) AS TotalOrderAmount
+SELECT c.CustomerID, c.CompanyName, SUM(od.unitPrice * od.Quantity) AS TotalOrderAmount
 	FROM Customers c
     JOIN Orders o
         ON c.CustomerID = o.CustomerID
@@ -11,7 +11,7 @@ SELECT c.CustomerID, c.CompanyName, o.OrderID, SUM(od.unitPrice * od.Quantity) A
 
 -- Using Implicit INNER JOIN
 
--- SELECT c.CustomerID, c.CompanyName, o.OrderID, SUM(od.unitPrice * od.Quantity) AS TotalOrderAmount
+-- SELECT c.CustomerID, c.CompanyName, SUM(od.unitPrice * od.Quantity) AS TotalOrderAmount
 -- 	FROM Customers c, Orders o, OrderDetails od
 --     WHERE c.CustomerID = o.CustomerID
 --        AND o.OrderID = od.OrderID
